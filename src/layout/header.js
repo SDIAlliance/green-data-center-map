99d94f0bbd146c038855612e33b2c3a9f7a21a87
Diogo Dias <diogovicentedias@gmail.com>
2022-03-23 11:41:47 +0000

    feat(header): remove extra links

--- src/layout/header.js
+++ src/layout/header.js
@@ -2,32 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 import SharedHeader from '../components/sharedheader';
-import OpenPositionsBadge from '../components/openpositionsbadge';
 
 const logo = resolvePath('images/electricitymap-logo.svg');
 
 const headerLinks = [
   {
-    label: 'Live',
-    active: true,
-  },
-  {
-    label: <React.Fragment>We&apos;re hiring!<OpenPositionsBadge /></React.Fragment>,
-    href: 'https://electricitymap.org/jobs#joboffers?utm_source=app.electricitymap.org&utm_medium=referral',
-  },
-
-  {
     label: 'Open Source',
     href: 'https://electricitymap.org/open-source?utm_source=app.electricitymap.org&utm_medium=referral',
-  },
-  {
-    label: 'Blog',
-    href: 'https://electricitymap.org/blog?utm_source=app.electricitymap.org&utm_medium=referral',
-  },
-  {
-    label: 'Get our data',
-    href: 'https://electricitymap.org?utm_source=app.electricitymap.org&utm_medium=referral',
-  },
+  }
 ];
 
 const Container = styled.div`
