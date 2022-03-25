4a2f9c0dd442601f92c800e9208b874c9095d116
Diogo Dias <diogovicentedias@gmail.com>
2022-03-25 16:52:12 +0000

    fix(data reducer): remove unnecessary variable name transformation

--- src/reducers/dataReducer.js
+++ src/reducers/dataReducer.js
@@ -80,8 +80,6 @@ module.exports = (state = initialDataState, action) => {
               energyOutputStreamEndpoint: dataCenterFacility.energy_output_stream_endpoint,
               energyOutputStreamTopic: dataCenterFacility.energy_output_stream_topic,
               equipmentInventoryRestEndpoint: dataCenterFacility.equipment_inventory_rest_endpoint,
-              lat: dataCenterFacility.geo_lat,
-              lng: dataCenterFacility.geo_lon,
               totalElectricalCapacity: dataCenterFacility.total_electrical_capacity,
               updatedAt: dataCenterFacility.updated_at
             }
